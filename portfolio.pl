0fb779ed0d992c5286eb6089a77f3a7a2f8949dd
Moritz Gellner <moritz.gellner@gmail.com>
2013-11-13 20:12:15 -0600

    minor changes

--- portfolio.pl
+++ portfolio.pl
@@ -155,12 +155,13 @@ elsif ($loggedin == 1) {
 						$pid = get_current_pid();
                         #Get parameters for pageview
                         my @currentAmount = eval { ExecSQL($dbuser, $dbpasswd, "SELECT amount FROM cash_accts WHERE owner = ? AND portfolio = ?", "COL", $username, $pid);};
+                        my @portfolioVal = eval { ExecSQL($dbuser,$dbpasswd,"SELECT sum(d.close * s.amount) FROM (SELECT * FROM stocks_new WHERE symbol IN (SELECT symbol FROM stocks WHERE portfolio = ?) UNION SELECT * FROM cs339.StocksDaily WHERE symbol IN (SELECT symbol FROM stocks WHERE portfolio = ?)) d INNER JOIN stocks s ON d.symbol = s.symbol WHERE d.symbol NOT IN (SELECT symbol FROM (SELECT * FROM stocks_new WHERE symbol IN (SELECT symbol FROM stocks WHERE portfolio = ?) UNION SELECT * FROM cs339.StocksDaily WHERE symbol IN (SELECT symbol FROM stocks WHERE portfolio = ?)) sd WHERE d.timestamp < s.timestamp)",'COL',$pid,$pid,$pid,$pid); };
                         
                         #TODO::: logic to calculate portfolio value, average volatility, and correlation
 
                         $overviewTemplate->param(
                                 CASH_IN_ACCT => $currentAmount[0],
-                                PORTFOLIO_VAL => 10000,
+                                PORTFOLIO_VAL => $portfolioVal[0],
                                 PORTFOLIO_AVG_VOL => 0.5,
                                 PORTFOLIO_AVG_CORR => 0.8
                         );
