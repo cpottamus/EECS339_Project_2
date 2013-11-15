89e4b182bf25dfe471264bd35f0a6c572cfc5e0d
Carson Potter <cp3192@gmail.com>
2013-11-15 16:22:20 -0600

    test

--- portfolio.pl
+++ portfolio.pl
@@ -332,7 +332,7 @@ elsif ($loggedin == 1) {
 				$totime2 = param('endDate2');
 				my $bSymbol = param('bSymbol');
 
-=Pod
+
 				if($fromtime2 ne undef and $totime2 ne undef){
 					($fromtime2, $totime2) = (parse_date($fromtime2), parse_date($totime2));
 					my @m = eval { ExecSQL($dbuser, $dbpasswd,"SELECT avg(close) FROM (SELECT * FROM stocks_new WHERE symbol = ? AND timestamp <= ? AND timestamp >= ? UNION SELECT * FROM cs339.StocksDaily WHERE symbol = ? AND timestamp <= ? AND timestamp >= ?)","COL", $bSymbol, $totime2,$fromtime2, $bSymbol, $totime2, $fromtime2);};
@@ -365,10 +365,10 @@ elsif ($loggedin == 1) {
 
 
 					my @beta = eval{ ExecSQL($dbuser, $dbpasswd, "SELECT sum((a.close - ?)*(b.close - ?))/(? - 1) FROM (SELECT * FROM stocks_new WHERE symbol = ? AND timestamp >= ? AND timestamp <= ? UNION SELECT * FROM cs339.StocksDaily where symbol = ? AND timestamp >= ? AND timestamp <= ?) a UNION (SELECT * FROM beta_data WHERE timestamp >= ? AND timestamp <= ?) b WHERE a.timestamp = b.timestamp", "COL", $mean, $betaMean[0], $recordLen[0], $bSymbol, $totime2, $fromtime2, $bSymbol, $totime2, $fromtime2);};
-=cut
+
 					$stockStatTemplate = param(
-						COEFF => 5,#$coeff,
-						BETA =>6 #$beta[0]
+						COEFF => $coeff,
+						BETA =>$beta[0]
 						)
 				}
 
