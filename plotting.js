a648bc82087ac12c27e42d8dab71b153c55ebd2e
Carson Potter <cp3192@gmail.com>
2013-11-14 18:45:33 -0600

    1

--- plotting.js
+++ plotting.js
@@ -31,14 +31,14 @@ function create_graph(){
 			pointColor : "rgba(220,220,220,1)",
 			pointStrokeColor : "#fff",
 			data : datac.yhist//[65,59,90,81,56,55,40]//Replace with data.yhist;
-		},
-		{
-			fillColor : "rgba(151,187,205,0.5)",
-			strokeColor : "rgba(151,187,205,1)",
-			pointColor : "rgba(151,187,205,1)",
-			pointStrokeColor : "#fff",
-			data : datac.yfut//[28,48,40,19,96,27,100]//Replace with data.yfut;
-		}
+		// },
+		// {
+		// 	fillColor : "rgba(151,187,205,0.5)",
+		// 	strokeColor : "rgba(151,187,205,1)",
+		// 	pointColor : "rgba(151,187,205,1)",
+		// 	pointStrokeColor : "#fff",
+		// 	data : datac.yfut//[28,48,40,19,96,27,100]//Replace with data.yfut;
+		// }
 	]
 }
 var options = {
