7c8a162fc4c01132092dbce0265e6f4d07c0a9f7
Carson Potter <cp3192@gmail.com>
2013-11-15 16:26:19 -0600

    test

--- portfolio.pl
+++ portfolio.pl
@@ -380,6 +380,9 @@ elsif ($loggedin == 1) {
 					$stockStatTemplate->param(corr_matrix => get_matrix_string(\@stocks,$fromtime,$totime));
 
 				}
+									# voodoo magic redirect
+									my $redirectUrl = "portfolio.pl?act=stockStats&pfname=$pfname";
+									print "Location: $redirectUrl\n\n";
                         		bake_cookie();
 				print $stockStatTemplate->output;
 			} else { # stockHistory
